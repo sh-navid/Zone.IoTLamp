85572368c5df361454cf5b1fca6a5bf1a3508b7f
sh-navid <shamshirsaz.navid@live.com>
2021-03-21 11:50:33 +0330

    update

--- IoTLampServer/IoTLampServer.ino
+++ IoTLampServer/IoTLampServer.ino
@@ -6,10 +6,52 @@ ESP8266WebServer server(80);
 
 const int led = 13;
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
+void root() {
+  String str = "";
+
+  str += "<!DOCTYPE html>";
+  str += "<html lang=\"en\">";
+  str += "<head>";
+  str += "    <meta charset=\"utf-8\" />";
+  str += "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable = no\" />";
+  str += "    <style type=\"text/css\">";
+  str += "        body {";
+  str += "            margin: 0;";
+  str += "            background-color: #212121;";
+  str += "        }";
+  str += "        btn {";
+  str += "            display: block;";
+  str += "            user-select: none;";
+  str += "            cursor: pointer;";
+  str += "            text-align: center;";
+  str += "            width: 3em;";
+  str += "            height: 3em;";
+  str += "            line-height: 3em;";
+  str += "            border-radius: 10em;";
+  str += "            background-color: royalblue;";
+  str += "            color: white;";
+  str += "            font-size: 200%;";
+  str += "            margin: 0 auto;";
+  str += "        }";
+  str += "        btn:nth-child(2) {";
+  str += "            background-color: hotpink;";
+  str += "        }";
+  str += "    </style>";
+  str += "</head>";
+  str += "<body>";
+  str += "  <a href=\"/blue\"><btn>☀</btn><a>";
+  str += "  <a href=\"/pink\"><btn>☀</btn><a>";
+  str += "</body>";
+  str += "</html>";
+
+  server.send(200, "text/html", str);
+}
+
+void lamp(int i) {
+  int k = digitalRead(i) + 1;
+  if (k > 1)k = 0;
+  digitalWrite(i, k);
+  root();
 }
 
 void setup(void) {
@@ -26,14 +68,18 @@ void setup(void) {
   Serial.println(WiFi.localIP());
   MDNS.begin("esp8266")
 
-  server.on("/", handleRoot);
+  server.on("/", root);
 
-  server.on("/inline", []() {
-    server.send(200, "text/html", "this works as well");
+  server.on("/blue", []() {
+    lamp(4);
+  });
+
+  server.on("/pink", []() {
+    lamp(6);
   });
 
   server.begin();
-  Serial.println("HTTP server started");
+  Serial.println("My Lamp Server Started");
 }
 
 void loop(void) {
